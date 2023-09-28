c5ef666cfee6380ced9f4f2ceebe16eb95c45d28
unikorm <adaled00@gmail.com>
2023-09-28 14:23:36 +0200

    tak mapa funguje vdaka ich docs a teraz pokracujeme so malym stylom a pridanim popup-ov

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 
+import "../../App.css";
 import Map from "../Main/Map";
 import Blog from "../Main/Blog";
 import Description from "../Main/Description";
--- src/components/Main/Map.js
+++ src/components/Main/Map.js
@@ -6,17 +6,18 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
     const center = [48.825, 19.391]; // Change to your desired center coordinates
-    const zoom = 7; // Adjust the zoom level
+    const zoom = 7 // Adjust the zoom level
     const markers = [
         { position: [49.825, 17.391], text: "Marker 1" },
         { position: [50.825, 21.391], text: "Marker 2" }
-    ]
+        ];
+
     return (
-        <MapContainer center={center} zoom={zoom} style={{height: `400px`, width: `100%`}}>
-            <TileLayer 
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        <MapContainer center={center} zoom={zoom} className="mapContainer">
+            <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-             />
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
             {markers.map((marker, index) => (
                 <Marker key={index} position={marker.position}>
                     <Popup>{marker.text}</Popup>
