2e3451cd0ea517f4f5c6680ca7404f8e59d6b872
unikorm <adaled00@gmail.com>
2023-09-28 15:43:01 +0200

    pridal zopar lokaci s popup-om s textom, teraz dalej, pokracujeme na dalsich veciach

--- src/components/Main/Map.js
+++ src/components/Main/Map.js
@@ -1,26 +1,32 @@
 
 import "../../App.css";
+import { markers } from "../../positions.js";
 
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+const PopupContent = ({ content }) => (
+  <div dangerouslySetInnerHTML={{ __html: content }} />
+);
+
+
+
+
 const Map = () => {
     const center = [48.825, 19.391]; // Change to your desired center coordinates
-    const zoom = 7 // Adjust the zoom level
-    const markers = [
-        { position: [49.825, 17.391], text: "Marker 1" },
-        { position: [50.825, 21.391], text: "Marker 2" }
-        ];
+    const zoom = 6 // Adjust the zoom level
 
     return (
-        <MapContainer center={center} zoom={zoom} className="mapContainer">
+        <MapContainer center={center} zoom={zoom} maxZoom="15" scrollWheelZoom={false} className="mapContainer">
             <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {markers.map((marker, index) => (
                 <Marker key={index} position={marker.position}>
-                    <Popup>{marker.text}</Popup>
+                    <Popup>
+                        <PopupContent content={marker.text}/>
+                    </Popup>
                 </Marker>
             ))}
         </MapContainer>
---
+++ src/positions.js
@@ -0,0 +1,10 @@
+
+export const markers = [
+    { position: [48.084935, 17.139982], text: "Tu sme spinkali vedľa cesty <br />keď sme cestovali do IT. <br /> A niekto mi tu ukradol šlapku, <br /> ale iba jednu..." },
+    { position: [47.076496, 15.577760], text: "Tu sme spinkali na parkovisku <br /> pri nemocnici asi... <br /> Auto bolo trochu pokazené, <br /> takže preto tam." },
+    { position: [48.707455, 14.927590], text: "Tu sme spinkali poslednú noc <br /> cestou z roadtripu do Nemecka, <br /> cestou sem nás naháňal nadržaný býk..." },
+    { position: [46.36826739049281, 14.084859889952476], text: "Tu sme spinkali dve noci po sebe <br /> na Veľkonočnom tripe do Slovinska. <br /> Začiatok jari a nádherné miesto." },
+    { position: [45.77317382364392, 13.527147980410941], text: "Tu sme spinkali pri mori <br />  na našom letnom tripe k moru, <br /> bolo príliš plytké a plné ľudí..."},
+    { position: [48.46852769912247, 18.882837500699537], text: "Tu sme spinkali prvý krát pri dalších ľuďoch, <br /> ktorý spali v aute, krásne tu bolo <br />  v tom srdci Slovenska..." },
+    { position: [48.883877008772174, 19.037511179589945], text: "Tu som spinkal už dávno s kamarátom, <br /> len tak, v divočine Velkej Fatry." }
+];
