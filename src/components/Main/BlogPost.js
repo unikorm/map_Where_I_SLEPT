22051a69f5ce7cf6f0a58c74f651a48fbefb4ff7
unikorm <adaled00@gmail.com>
2023-10-05 11:08:22 +0200

    finnally find good icons, now styling blog boxs to make it pretty, then another stylling and filling data file with more data

--- src/components/Main/BlogPost.js
+++ src/components/Main/BlogPost.js
@@ -1,23 +1,23 @@
 
 import styles from "../../styles/blogPost.module.css";
 import postCover from "../../images/postCoverTwo.webp";
-import dateLogo from "../../images/calendar-19.svg";
-import placeLogo from "../../images/bed-19.svg";
+import dateLogo from "../../images/calendar-24.png";
+import placeLogo from "../../images/bed-20.png";
 
 const BlogPost = ({ post }) => {
     return (
         <article className={styles.postUnit}>
             <img src={postCover} alt="post image" className={styles.postCoverImage}/>
-            <section>
+            <section className={styles.postUnitTitle}>
                 <h3>{post.title}</h3>
                 <p>{post.place}</p>
             </section>
-            <aside>
-                <div>
+            <aside className={styles.postUnitInfo}>
+                <div className={`${styles.postUnitInfoSection} ${styles.date}`}>
                     <img src={dateLogo} alt="logo kalendaru pre datum kedy" />
                     <p>{post.date}</p>
                 </div>
-                <div>
+                <div className={`${styles.postUnitInfoSection} ${styles.how}`}>
                     <img src={placeLogo} alt="logo pre sposob spania"/>
                     <p>{post.how}</p>
                 </div>
