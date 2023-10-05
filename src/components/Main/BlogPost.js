f064c9b60ee60ac327eb11f949b71229fa82c224
unikorm <adaled00@gmail.com>
2023-10-05 12:54:01 +0200

    date formatting is done

--- src/components/Main/BlogPost.js
+++ src/components/Main/BlogPost.js
@@ -4,7 +4,14 @@ import postCover from "../../images/postCoverTwo.webp";
 import dateLogo from "../../images/calendar-24.png";
 import placeLogo from "../../images/bed-20.png";
 
+const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+}
+
 const BlogPost = ({ post }) => {
+    const formattedDate = formatDate(post.date);
+
     return (
         <article className={styles.postUnit}>
             <img src={postCover} alt="post image" className={styles.postCoverImage}/>
@@ -16,14 +23,14 @@ const BlogPost = ({ post }) => {
                 <div className={styles.postUnitInfoSection}>
                     <img src={dateLogo} alt="logo kalendaru pre datum kedy" />
                     <div className={styles.sectionWhen}>
-                        <p>Kedy</p>
-                        <p>{post.date}</p>
+                        <p>When</p>
+                        <p>{formattedDate}</p>
                     </div>
                 </div>
                 <div className={styles.postUnitInfoSection}>
                     <img src={placeLogo} alt="logo pre sposob spania"/>
                     <div className={styles.sectionHow}>
-                        <p>Ako</p>
+                        <p>How</p>
                         <p>{post.how}</p>
                     </div>
                 </div>
