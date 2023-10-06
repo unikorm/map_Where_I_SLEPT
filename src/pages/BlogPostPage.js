b9f96a9195dca872184434e87f696f30f3ae5986
unikorm <adaled00@gmail.com>
2023-10-06 18:16:01 +0200

    fukin styling, god damn it.. i want coding...

--- src/pages/BlogPostPage.js
+++ src/pages/BlogPostPage.js
@@ -1,5 +1,7 @@
 
 import blogData from "../blogData.json";
+import styles from "../styles/blogPostPage.module.css";
+import postCover from "../images/default.webp";
 
 import React from "react";
 import { useParams } from "react-router-dom";
@@ -7,24 +9,30 @@ import { useParams } from "react-router-dom";
 const BlogPostPage = () => {
   const { id } = useParams(); // Get the post ID from the URL
   const post = blogData.posts.find((post) => post.id === parseInt(id, 10));
+  const mainImagePath = process.env.PUBLIC_URL + post.imagePath || postCover;
 
   if (!post) {
     return <div>cosi je na**cu</div>; // Handle the case when the post is not found
   }
 
   return (
-    <section>
-        <div>
-            <h1>{post.title}</h1>
-            <p>{post.place}</p>
-            <p>{post.date}</p>
-            <p>{post.how}</p>
-        </div>
+    <section className={styles.blogPostPage}>
+      <div className={styles.divImage}>
+        <img src={mainImagePath} className={styles.mainImage} />
+      </div>
+      <section className={styles.mainSection}>
         <article>
-            <p>{post.description}</p>
-            <p>{post.content}</p>
-            <p>{post.info}</p>
+          <h1>{post.title}</h1>
+          <p>{post.place}</p>
+          <p>{post.date}</p>
+          <p>{post.how}</p>
         </article>
+        <article>
+          <p>{post.description}</p>
+          <p>{post.content}</p>
+          <p>{post.info}</p>
+        </article>
+      </section>
     </section>
   );
 };
