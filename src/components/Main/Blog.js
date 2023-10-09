9f95a34bca0659dded35659f4eefe6277c8f190a
unikorm <adaled00@gmail.com>
2023-10-10 00:47:19 +0200

    pridal logiku na zobrazovania viac postov na blogu, zatial nefunguje... damnit

--- src/components/Main/Blog.js
+++ src/components/Main/Blog.js
@@ -3,23 +3,41 @@ import styles from "../../styles/blog.module.css";
 import BlogPost from "./BlogPost";
 import blogData from "../../blogData.json";
 
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Blog = () => {
-    const posts = blogData.posts;
+    const initialPostCount = 12;
+    const posts = blogData.posts.slice(0, initialPostCount);
+
+    const [displayCount, setDisplayCount] = useState(initialPostCount);
+
+    const loadMorePosts = () => {
+        const newDisplayCount = displayCount + 12;
+        if (newDisplayCount < blogData.posts.length) {
+            setDisplayCount(newDisplayCount);
+        };
+    };
 
     return (
         <section className={styles.blogSection}>
             <h3 className={styles.h3}>More about places where I/We slept</h3>
             <section className={styles.postsTable}>
-                {posts.map(( post ) => (
-                    post.id <= 12 && (
-                        <Link to={`/${post.id}`} key={post.id} className={styles.postBox}>
-                            <BlogPost post={post} />
-                        </Link>
-                    )))
-                }
+            {posts.map((post) => (
+                <Link to={`/${post.id}`} key={post.id} className={styles.postBox}>
+                    <BlogPost post={post} />
+                </Link>
+            ))}
             </section>
+            {displayCount < blogData.posts.length && (
+                <button 
+                    onClick={loadMorePosts}
+                    className={styles.loadMoreBttn}
+                    key="load-more-button"
+                >
+                    Load More
+                </button>
+            )}
         </section>
     );
 }
