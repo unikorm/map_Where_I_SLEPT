a03e9740f01351536776398a2a2382b8e9bfbb8c
unikorm <adaled00@gmail.com>
2023-10-10 13:23:22 +0200

    snazil som sa zanimovat troska pridanie novych blog postov ale nejde mi to, zatial necham tak, teraz pokracujeme

--- src/components/Main/Blog.js
+++ src/components/Main/Blog.js
@@ -12,10 +12,12 @@ const Blog = () => {
         return blogData.posts.slice(0, initialPostCount);
     });
 
+    const [displayedPostBoxes, setDisplayedPostBoxes] = useState([]);
+
     const loadMorePosts = () => {
         const newDisplayCount = displayedPosts.length + 12;
         if (newDisplayCount <= blogData.posts.length) {
-            setDisplayedPosts(blogData.posts.slice(0, newDisplayCount));
+          setDisplayedPosts(blogData.posts.slice(0, newDisplayCount));
         }
     };
 
@@ -23,8 +25,10 @@ const Blog = () => {
         <section className={styles.blogSection}>
             <h3 className={styles.h3}>More about places where I/We slept</h3>
             <section className={styles.postsTable}>
-            {displayedPosts.map((post) => (
-                <Link to={`/${post.id}`} key={post.id} className={styles.postBox}>
+            {displayedPosts.map((post, index) => (
+                <Link to={`/${post.id}`} key={post.id} className={`${styles.postBox}
+                ${ index >= 0 && styles.show
+                  }`}>
                     <BlogPost post={post} />
                 </Link>
             ))}
