b22dd4a75ea7f9e33e3667d816ff69fb1f7822a4
unikorm <adaled00@gmail.com>
2023-10-30 22:36:57 +0100

    to iste na footeri

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,35 +2,23 @@
 import styles from "../styles/footer.module.css";
 import logo from "../images/forest-96.svg";
 import GitHubLogo from "../images/github-40.svg";
-import { useActivePage } from "../customHooks/useActivePage";
 
 import { Link } from 'react-router-dom';
 
-const scrollToTop = () => {
-    window.scrollTo({top: 0, behavior: "smooth"});
-};
+// const scrollToTop = () => {
+//     window.scrollTo({top: 0, behavior: "smooth"});
+// };
 
 const Footer = () => {
-    const { activePage, setActive } = useActivePage();
-
-    const includeScrollTOTop = activePage === "main";
-    const isAnotherPageActive = activePage === "aboutMe";
-
-    const handleAboutMeClick = () => {
-        setActive("aboutMe");
-    };
 
     return (
         <footer>
             <section className={styles.logoInFooter}>
                 <div className={styles.logoSectionLeft}>
-                    <Link to="/" onClick={() => {
-                        setActive("main");
-                        if (includeScrollTOTop) {
-                            scrollToTop();
-                        }}} className={styles.logoLinkStyle}>
-                            <img src={logo} alt="Logo"/>
-                            <p>myPATH</p>
+                    <Link to="/"
+                        className={styles.logoLinkStyle}>
+                        <img src={logo} alt="logo"/>
+                        <p>myPATH</p>
                     </Link>
                 </div>
             </section>
@@ -43,8 +31,7 @@ const Footer = () => {
                     </li>
                     <li>
                         <Link to="/aboutMe"
-                        onClick={handleAboutMeClick}
-                        className={`${styles.linkStyleMenu} ${isAnotherPageActive ? styles["active-link"] : ""}`}>
+                            className={styles.linkStyleMenu}>
                             About US
                         </Link>
                     </li>
