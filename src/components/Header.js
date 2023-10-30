7e0c2a1aa643ca357c428239ca7b2b2ef9717821
unikorm <adaled00@gmail.com>
2023-10-30 22:35:05 +0100

    tym ze som vyhodil activepage tak som znefunkcnil to sparovane tlacidlo co meni farbu n aabout us, teraz som vymazal logiku z header sekcie, ideme na dalsie

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,30 @@
 
 import styles from "../styles/header.module.css";
 import logo from "../images/forest-48.svg";
-import { useActivePage } from "../customHooks/useActivePage";
 
 import { Link } from 'react-router-dom';
 
 
-const scrollToTop = () => {
-    window.scrollTo({top: 0, behavior: "smooth"});
-};
+// const scrollToTop = () => {
+//     window.scrollTo({top: 0, behavior: "smooth"});
+// };
 
 const Header = () => {
-    const { activePage, setActive } = useActivePage();
-
-    const includeScrollTOTop = activePage === "main";
-    const isAnotherPageActive = activePage === "aboutMe";
-
-    const handleAboutMeClick = () => {
-        setActive("aboutMe");
-    };
 
     return (
         <header className={styles.header}>
             <nav className={styles.navigation}>
                 <div className={styles.logoSectionLeft}>
-                    <Link to="/" onClick={() => {
-                        setActive("main");
-                        if (includeScrollTOTop) {
-                            scrollToTop();
-                        }}} className={styles.logoLinkStyle}>
-                            <img src={logo}/>
-                            <p>myPATH</p>
+                    <Link to="/"
+                        className={styles.logoLinkStyle}>
+                        <img src={logo} alt="logo" />
+                        <p>myPATH</p>
                     </Link>
                 </div>
                 <ul className={styles.menuNavigationCentre}>
                     <li>
                         <Link to="/aboutMe"
-                        onClick={handleAboutMeClick}
-                        className={`${styles.linkStyleMenu} ${isAnotherPageActive ? styles["active-link"] : ""}`}>
+                            className={styles.linkStyleMenu}>
                             About US
                         </Link>
                     </li>
