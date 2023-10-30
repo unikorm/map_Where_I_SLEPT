bd550d3b67eccfa69860e61cac083c18e69aea9e
unikorm <adaled00@gmail.com>
2023-10-30 22:38:49 +0100

    tak uz aj na about, a funguje to, akurat styling trochu v riti, co sa tyka footeru ale to spojazdnim

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,20 +2,13 @@
 import { Link } from "react-router-dom";
 
 import styles from "../styles/about.module.css";
-import { useActivePage } from "../customHooks/useActivePage"; 
 
 const AboutMe = () => {
-    const { activePage, setActive } = useActivePage();
-    
-    const handleBackClick = () => {
-        setActive("main");
-    };
 
     return (
         <section className={styles.aboutMeSection}>
             <p>e to vsjo...</p>
-            <Link to="/"
-            onClick={handleBackClick}>
+            <Link to="/">
                 ⬅️
             </Link>
         </section>
