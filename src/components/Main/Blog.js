6e2ddd8a6bdf02366fece2636de64d4a7b4430c4
unikorm <adaled00@gmail.com>
2023-10-31 19:12:51 +0100

    super, uz budu len posts ktore su true takze tak

--- src/components/Main/Blog.js
+++ src/components/Main/Blog.js
@@ -8,18 +8,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Blog = () => {
-    const initialPostCount = 6;
-    const posts = blogData.posts.slice(0, initialPostCount);
-
-    // dobre, toto bude inak, ja dam na mapu vsetky miesta kde som spal, ale blog section bude iba s miestami co si 
-    // vyberiem ze stoja za to, takto na zaciatok, lebo tolko contentu nenapisem, takze asi mozno max 10 to bude,
-    // potom casom mozno viac... chcem programovat, nie pisat blog...
+    const filteredPosts = blogData.posts.filter((post) => post.show === true);
+    
 
     return (
         <section className={styles.blogSection}>
             <h3 className={styles.h3}>More about places where I/We slept</h3>
             <section className={styles.postsTable}>
-            {posts.map((post) => (
+            {filteredPosts.map((post) => (
                 <Link to={`/${post.id}`}
                 key={post.id} className={styles.postBox}>
                     <BlogPost post={post} />                 { /* component */ }
